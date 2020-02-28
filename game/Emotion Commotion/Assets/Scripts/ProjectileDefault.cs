f7fcbc706a298f9bdaea585e3349d0afc08dd424
Jordan Durci <durcij@aldenv177.allegheny.edu>
2020-02-28 15:11:32 -0500

    Finished the projectile scripting for now.

--- game/Emotion Commotion/Assets/Scripts/ProjectileDefault.cs
+++ game/Emotion Commotion/Assets/Scripts/ProjectileDefault.cs
@@ -5,58 +5,85 @@ using UnityEngine;
 public class ProjectileDefault : MonoBehaviour {
 
 	public float speed;
+	bool isFiredLeft = false;
+	bool isFiredRight = false;
+	bool isFiredForward = false;
+	bool isFiredBackward = false;
 	bool isFired = false;
 	bool alive = true;
 	Animator anim;
 	float timer;
 	public float lifespan = 0.5f;
-	PlayerMovementDefault PlayerData = new PlayerMovementDefault();
-	Vector3 originalPosition;
+	Vector3 spawnPosition;
+	GameObject player;
+	PlayerMovementDefault playerData;
 
 
 	// Use this for initialization
 	void Awake () {
 		anim = GetComponent <Animator> ();
 
-		originalPosition = gameObject.transform.position; // Sets the projectile's original position
+		player = GameObject.Find("Risio");
+
+		speed = 3.0f;
+
+		playerData = player.GetComponent<PlayerMovementDefault>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		speed = 5.0f;
-
 		CheckLifespan ();
 
 		// New if based on player attack methods from before?
-		if (!isFired) {
-			if (PlayerData.isAttackingForward) {
-				isFired = true;
+
+		if (!isFired){
+			spawnPosition = player.transform.position; // Sets the projectile's spawn position
+			AnimatingProjectile(isFired);
+			transform.position = spawnPosition;
+			if (playerData.isAttackingLeft) {
+				isFiredLeft = true;
+			} else if (playerData.isAttackingRight) {
+				isFiredRight = true;
+			} else if (playerData.isAttackingForward) {
+				isFiredForward = true;
+			} else if (playerData.isAttackingBackward) {
+				isFiredBackward = true;
+			}
+		}
+
+		if (isFiredLeft || isFiredRight || isFiredForward || isFiredBackward) {
+			isFired = true;
+		}
+
+		if (isFired) {
+			if (isFiredForward) {
 				transform.Translate(Vector3.down * Time.deltaTime * speed);
 				AnimatingProjectile(isFired);
-			} else if (PlayerData.isAttackingBackward) {
-				isFired = true;
+			} else if (isFiredBackward) {
 				transform.Translate(Vector3.up * Time.deltaTime * speed);
 				AnimatingProjectile(isFired);
-			} else if (PlayerData.isAttackingLeft) {
-				isFired = true;
+			} else if (isFiredLeft) {
 				transform.Translate(Vector3.left * Time.deltaTime * speed);
 				AnimatingProjectile(isFired);
-			} else if (PlayerData.isAttackingRight) {
-				isFired = true;
+			} else if (isFiredRight) {
 				transform.Translate(Vector3.right * Time.deltaTime * speed);
 				AnimatingProjectile(isFired);
 			}
-		} else {
-			// Kill projectile animation
-			AnimatingProjectile(isFired);
 		}
 	}
 
 	void CheckLifespan () {
-		timer += Time.deltaTime;
-		if (timer >= lifespan) {
-			isFired = false;
+		if (isFired) {
+			timer += Time.deltaTime;
+			if (timer >= lifespan) {
+				isFired = false;
+				isFiredLeft = false;
+				isFiredRight = false;
+				isFiredForward = false;
+				isFiredBackward = false;
+				timer = 0.0f;
+			}
 		}
 	}
 
