d029d6e2bbdd9e4def35b09d7fe68818c459f6b1
Jordan Durci <durcij@aldenv177.allegheny.edu>
2020-02-28 16:20:29 -0500

    Began to create the crates, nullos, and the border of the game.

---
+++ game/Emotion Commotion/Assets/Scripts/Crate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour {
+
+	bool intact;
+	Animator anim;
+	AudioSource crateBreak;
+	GameObject attackRisio;
+	CircleCollider2D hurtBox;
+
+
+	// Use this for initialization
+	void Start () {
+		intact = true;
+		anim = GetComponent <Animator> ();
+		crateBreak = GetComponent<AudioSource> ();
+		attackRisio = GameObject.Find("RisioAttack");
+		hurtBox = attackRisio.GetComponent<CircleCollider2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
