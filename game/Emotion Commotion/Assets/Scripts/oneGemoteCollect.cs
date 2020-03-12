56ecdda6c2a3b10304e6eabcb20a302b0cfff6b7
Jordan Durci <durcij@aldenv177.allegheny.edu>
2020-03-12 16:08:09 -0400

    Collectible goes away now.

--- game/Emotion Commotion/Assets/Scripts/oneGemoteCollect.cs
+++ game/Emotion Commotion/Assets/Scripts/oneGemoteCollect.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class oneGemoteCollect : MonoBehaviour {
 
+	AudioSource collected;
+  float timer;
+  float soundWait;
+  bool destroy = false;
+
 	// Use this for initialization
 	void Start () {
-		
+		collected = GetComponent<AudioSource>();
+    timer = 0.0f;
+    soundWait = 0.5f;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+    if (destroy) {
+      timer += Time.deltaTime;
+      if (timer >= soundWait) {
+        Destroy(gameObject);
+      }
+    }
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.tag == "Player") {
+      collected.Play();
+      destroy = true;
+		}
 	}
 }
