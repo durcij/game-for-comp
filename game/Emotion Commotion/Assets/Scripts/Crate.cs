7876c26d944ff68106a2ce0102a786567472d278
Jordan Durci <durcij@aldenv177.allegheny.edu>
2020-03-16 13:06:36 -0400

    Crates spawn gemotes!  Now placing them in the map.

--- game/Emotion Commotion/Assets/Scripts/Crate.cs
+++ game/Emotion Commotion/Assets/Scripts/Crate.cs
@@ -38,9 +38,14 @@ public class Crate : MonoBehaviour {
 			timer += Time.deltaTime;
 			if (timer >= breakWait) {
 				broken = true;
+				timer = 0.0f;
 			}
 		} else if (broken) {
 			anim.SetBool ("broken", broken);
+			timer += Time.deltaTime;
+			if (timer >= breakWait) {
+				Destroy(gameObject);
+			}
 		}
 	}
 
@@ -48,13 +53,18 @@ public class Crate : MonoBehaviour {
 		if (intact) {
 			if (other.tag == "Risio Projectile" || other.tag == "Furia Projectile" || other.tag == "Tristitia Projectile" || other.tag == "Dormio Projectile" || other.tag == "Dilectio Projectile" || other.tag == "Verecundia Projectile" || other.tag == "Invidia Projectile" || other.tag == "Mercuria Projectile") {
 				intact = false;
+				SpawnGemotes();
 			}
 		}
 	}
 
 	void SpawnGemotes() {
-		float randomPosition = (Random.Range(2, 4) / 100); // Can't be a float... find solution
-		Debug.Log(randomPosition);
-		Instantiate(oneGemote, randomPosition, 0);
+		// Generates six gemotes (totalling 23 gemotes) at random positions near the crate
+		Instantiate(oneGemote, new Vector3((transform.position.x + Random.Range(-0.28f, 0.28f)), ((transform.position.y - 0.32f) + Random.Range(-0.32f, 0.32f)), 0), Quaternion.identity);
+		Instantiate(oneGemote, new Vector3((transform.position.x + Random.Range(-0.28f, 0.28f)), ((transform.position.y - 0.32f) + Random.Range(-0.32f, 0.32f)), 0), Quaternion.identity);
+		Instantiate(oneGemote, new Vector3((transform.position.x + Random.Range(-0.28f, 0.28f)), ((transform.position.y - 0.32f) + Random.Range(-0.32f, 0.32f)), 0), Quaternion.identity);
+		Instantiate(fiveGemotes, new Vector3((transform.position.x + Random.Range(-0.28f, 0.28f)), ((transform.position.y - 0.32f) + Random.Range(-0.32f, 0.32f)), 0), Quaternion.identity);
+		Instantiate(fiveGemotes, new Vector3((transform.position.x + Random.Range(-0.28f, 0.28f)), ((transform.position.y - 0.32f) + Random.Range(-0.32f, 0.32f)), 0), Quaternion.identity);
+		Instantiate(tenGemotes, new Vector3((transform.position.x + Random.Range(-0.28f, 0.28f)), ((transform.position.y - 0.32f) + Random.Range(-0.32f, 0.32f)), 0), Quaternion.identity);
 	}
 }
