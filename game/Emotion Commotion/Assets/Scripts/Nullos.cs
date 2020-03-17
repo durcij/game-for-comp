686db3ab13426038b21c6972445297296b86d69c
Jordan Durci <durcij@aldenv177.allegheny.edu>
2020-03-17 13:42:56 -0400

    Began declaring variables for the Nullos.

--- game/Emotion Commotion/Assets/Scripts/Nullos.cs
+++ game/Emotion Commotion/Assets/Scripts/Nullos.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Nullos : MonoBehaviour {
 
+	float timer;
+	float deathWait;
+	
+	bool runningLeft;
+	bool runningRight;
+	bool runningForward;
+	bool runningBackward;
+
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
